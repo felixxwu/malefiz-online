46a789f4cb66e50e52ea2f644c87c47c84505be1
Felix Wu <fwu@shipserv.com>
2023-07-17 18:16:44 +0100

    lighthouse improvements

--- src/listeners/addPointerEventListeners.ts
+++ src/listeners/addPointerEventListeners.ts
@@ -20,27 +20,31 @@ export function addPointerEventListeners() {
       }
     }
   })
-  svg!.addEventListener('pointermove', event => {
-    if (!store.mouseDownData) return
+  svg!.addEventListener(
+    'pointermove',
+    event => {
+      if (!store.mouseDownData) return
 
-    const index = evCache.findIndex(cachedEv => cachedEv.pointerId === event.pointerId)
-    evCache[index] = event
-    store.svgTransition = 0
-    translateGroup!.style.pointerEvents = 'none'
+      const index = evCache.findIndex(cachedEv => cachedEv.pointerId === event.pointerId)
+      evCache[index] = event
+      store.svgTransition = 0
+      translateGroup!.style.pointerEvents = 'none'
 
-    if (evCache.length === 2) {
-      const euclideanDistance = getDistance(evCache[0], evCache[1])
-      const zoomDiff = euclideanDistance / store.mouseDownData.pinchDistance
-      store.svgZoom = store.mouseDownData.svgZoom * zoomDiff
-    } else {
-      const xDiff = event.clientX - store.mouseDownData.coords.x
-      const yDiff = event.clientY - store.mouseDownData.coords.y
-      store.svgTranslation = {
-        x: store.mouseDownData.svgTranslation.x + xDiff / store.svgZoom,
-        y: store.mouseDownData.svgTranslation.y + yDiff / store.svgZoom,
+      if (evCache.length === 2) {
+        const euclideanDistance = getDistance(evCache[0], evCache[1])
+        const zoomDiff = euclideanDistance / store.mouseDownData.pinchDistance
+        store.svgZoom = store.mouseDownData.svgZoom * zoomDiff
+      } else {
+        const xDiff = event.clientX - store.mouseDownData.coords.x
+        const yDiff = event.clientY - store.mouseDownData.coords.y
+        store.svgTranslation = {
+          x: store.mouseDownData.svgTranslation.x + xDiff / store.svgZoom,
+          y: store.mouseDownData.svgTranslation.y + yDiff / store.svgZoom,
+        }
       }
-    }
-  })
+    },
+    { passive: true }
+  )
   svg!.addEventListener('pointerup', onPointerUp)
   svg!.addEventListener('pointerleave', onPointerUp)
   svg!.addEventListener('pointercancel', onPointerUp)
--- src/listeners/addWheelEventListeners.ts
+++ src/listeners/addWheelEventListeners.ts
@@ -3,18 +3,22 @@ import { svg } from '../utils/getSvgGroup'
 import { store } from '../data/store'
 
 export function addWheelEventListeners() {
-  svg!.addEventListener('wheel', event => {
-    store.svgTransition = 0
-    if (event.ctrlKey) {
-      store.svgZoom = Math.min(
-        Math.max(CONSTS.MIN_ZOOM, store.svgZoom * (1 + event.deltaY / CONSTS.ZOOM_SLOWDOWN)),
-        CONSTS.MAX_ZOOM
-      )
-    } else {
-      store.svgTranslation = {
-        x: store.svgTranslation.x - event.deltaX / store.svgZoom / CONSTS.PAN_SLOWDOWN,
-        y: store.svgTranslation.y - event.deltaY / store.svgZoom / CONSTS.PAN_SLOWDOWN,
+  svg!.addEventListener(
+    'wheel',
+    event => {
+      store.svgTransition = 0
+      if (event.ctrlKey) {
+        store.svgZoom = Math.min(
+          Math.max(CONSTS.MIN_ZOOM, store.svgZoom * (1 + event.deltaY / CONSTS.ZOOM_SLOWDOWN)),
+          CONSTS.MAX_ZOOM
+        )
+      } else {
+        store.svgTranslation = {
+          x: store.svgTranslation.x - event.deltaX / store.svgZoom / CONSTS.PAN_SLOWDOWN,
+          y: store.svgTranslation.y - event.deltaY / store.svgZoom / CONSTS.PAN_SLOWDOWN,
+        }
       }
-    }
-  })
+    },
+    { passive: true }
+  )
 }
