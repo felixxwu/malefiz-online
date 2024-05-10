c60b7de19884816aa324029d52c52d46dc76a9ba
Felix Wu <fwu@shipserv.com>
2024-05-10 19:38:02 +0100

    Update colours

--- src/config/colours.ts
+++ src/config/colours.ts
@@ -2,5 +2,5 @@ export const colours = {
   background: 'hsl(75deg 30% 85%)',
   safeCircle: 'hsl(75deg 3% 30%)',
   highlight: 'hsl(0deg 3% 30%)',
-  neutralPlayer: '#ddd',
+  neutralPlayer: 'hsl(75deg 30% 75%)',
 }
