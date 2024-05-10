1fe8e58e294a23b20a7b02b7dd173fa3dee03939
Felix Wu <fwu@shipserv.com>
2024-05-10 17:00:02 +0100

    Randomly pick enemy models using today's date as a seed

--- src/utils/players.ts
+++ src/utils/players.ts
@@ -1,12 +1,30 @@
+import { eyesList } from '../playermodel/eyes'
+import { headList } from '../playermodel/heads'
+import { mouthList } from '../playermodel/mouthes'
 import { PlayerModel } from '../types/gameTypes'
 
+let today = new Date().setHours(0, 0, 0, 0) / 1000000
+
+function getModelNum(mod: number) {
+  today = Math.floor(today * 0.9)
+  return today % mod
+}
+
+function getModel() {
+  return {
+    eyes: getModelNum(eyesList.length),
+    head: getModelNum(headList.length),
+    mouth: getModelNum(mouthList.length),
+  }
+}
+
 export const players: PlayerDefs[] = [
-  { id: '1', colour: 'hsl(0 55% 65%)', name: 'Red', model: { eyes: 0, mouth: 0, head: 0 } },
-  { id: '2', colour: 'hsl(240 55% 75%)', name: 'Blue', model: { eyes: 0, mouth: 0, head: 0 } },
-  { id: '3', colour: 'hsl(140 55% 60%)', name: 'Green', model: { eyes: 0, mouth: 0, head: 0 } },
-  { id: '4', colour: 'hsl(60 55% 60%)', name: 'Yellow', model: { eyes: 0, mouth: 0, head: 0 } },
-  { id: '5', colour: 'hsl(300 55% 60%)', name: 'Purple', model: { eyes: 0, mouth: 0, head: 0 } },
-  { id: '6', colour: 'hsl(30 55% 60%)', name: 'Orange', model: { eyes: 0, mouth: 0, head: 0 } },
+  { id: '1', colour: 'hsl(0 55% 65%)', name: 'Red', model: getModel() },
+  { id: '2', colour: 'hsl(240 55% 75%)', name: 'Blue', model: getModel() },
+  { id: '3', colour: 'hsl(140 55% 60%)', name: 'Green', model: getModel() },
+  { id: '4', colour: 'hsl(60 55% 60%)', name: 'Yellow', model: getModel() },
+  { id: '5', colour: 'hsl(300 55% 60%)', name: 'Purple', model: getModel() },
+  { id: '6', colour: 'hsl(30 55% 60%)', name: 'Orange', model: getModel() },
 ]
 
 export type PlayerDefs = {
